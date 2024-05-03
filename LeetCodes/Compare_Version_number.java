be3ffedb87305c72bb1b4d194f797c3ea62ec61d
Dnyaneshwarkangude <100485026+Dnyaneshwarkangude@users.noreply.github.com>
2024-05-03 12:48:56 +0530

    Update Compare_Version_number.java

--- LeetCodes/Compare_Version_number.java
+++ LeetCodes/Compare_Version_number.java
@@ -40,6 +40,8 @@ version1 and version2 only contain digits and '.'.
 version1 and version2 are valid version numbers.
 All the given revisions in version1 and version2 can be stored in a 32-bit integer.
 */
+
+// My Solution : 
 import java.util.*;
 class Solution {
     public static void main(String[] art){
@@ -96,6 +98,30 @@ class Solution {
             }
             if(flag){
                 return -1;
+            }
+        }
+        return 0;
+    }
+}
+
+// Optimized code from leetcode user(someone )
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        int n = version1.length(), m = version2.length();
+        int i = 0, j = 0;
+        while (i < n || j < m) {
+            int x = 0;
+            for (; i < n && version1.charAt(i) != '.'; ++i) {
+                x = x * 10 + version1.charAt(i) - '0';
+            }
+            ++i; // 跳过点号
+            int y = 0;
+            for (; j < m && version2.charAt(j) != '.'; ++j) {
+                y = y * 10 + version2.charAt(j) - '0';
+            }
+            ++j; // 跳过点号
+            if (x != y) {
+                return x > y ? 1 : -1;
             }
         }
         return 0;
